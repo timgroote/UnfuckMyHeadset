1a8c2f4f8964b8b8ec25159d5852604744b84962
Tim Groote <timgroote@gmail.com>
2025-06-03 10:26:40 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # UnfuckMyHeadset
 automatically disable telephony service on bluetooth headsets to prevent audio stutter in games
+
+## why?
+Often, bluetooth headsets have issues like stuttering, popping or skipping every few seconds, or even switching to low bandwidth
+mode to allow use of the microphone when playing games.
+Generally speaking, this is considered a bad thing.
+
+The bluetooth device settings in windows are horribly inaccessible and take a lot of time to find
+To make matters worse, every time you reboot your computer you need to find the settings again
+and turn the service off because it will turn itself back on.
+
+UnfuckMyHeadset aims to provide a one-click (or even zero click) solution to fixing that problem.
+
+## how?
+This dotnet console application enumerates your paired bluetooth devices and tries to identify
+bluetooth headsets and headphones.
+If any of them have the remote telephony service enabled, this service will disable them.
+The program will then terminate.
---
+++ UnfuckMyHeadset/Program.cs
@@ -0,0 +1,35 @@
+﻿using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+
+namespace UnfuckMyHeadset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Finding headset!");
+            BluetoothClient btc = new BluetoothClient();
+            foreach(var d in btc.PairedDevices)
+            {
+                Console.WriteLine("Checking {0}", d.DeviceName);
+
+                if(
+                    d.ClassOfDevice.Device == DeviceClass.AudioVideoHeadphones || 
+                    d.ClassOfDevice.Device == DeviceClass.AudioVideoHandsFree || 
+                    d.ClassOfDevice.Device == DeviceClass.AudioVideoHeadset 
+                )
+                {
+                    Console.WriteLine("Headset found.");
+
+					if (d.InstalledServices.Contains(BluetoothService.Handsfree))
+                    {
+						Console.WriteLine("Unfucking headset!");
+					    d.SetServiceState(BluetoothService.Handsfree, false);
+				    }
+				}
+			}
+
+            Console.WriteLine("Headset unfucked!");
+		}
+    }
+}
